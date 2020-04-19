211055f24994e1361b9e1ddca6e098a039e8c4a5
jiayanwang <jiayan.wang@mail.utoronto.com>
2020-04-19 03:58:24 -0400

    update decription

--- R/getSurvData.R
+++ R/getSurvData.R
@@ -1,4 +1,18 @@
-# tidy column type - survival status
+
+#' to creat a record of censoring event into numeric value
+#'
+#' A function to convert the censoring event into numeric
+#' value for surivival analysis
+#'
+#' @param x event
+#'
+#' @return return the result if the x event is censored
+#'
+#' @example
+#'
+#' recordstatus("Alive")
+#'
+#' @export
 recordstatus <- function(x){
   if(grepl("^[dDy]",x )) {rs = 1}
   if(grepl("^[aAn]",x )){rs = 0}
@@ -13,11 +27,11 @@ NYstatus <- function(x){
 
 getCoxEfficient <- function(gseid){
   coxdata <- clean_data_db[[gseid]]
-  
-  #remove unknown status 
+
+  #remove unknown status
   status_name <- names(coxdata)[grep("status",names(coxdata))][1]
-  
-  
+
+
   if (gseid == "GSE37745" | gseid == "gse37745") {
     status_name <- "dead.ch1"
   }
@@ -25,32 +39,32 @@ getCoxEfficient <- function(gseid){
     discard <- which(coxdata[,status_name]=="Not available" | coxdata[,status_name]=="NTL" )
     coxdata<- coxdata[-discard,]
   }
-  
-  
+
+
   #record status for censored event
   if(typeof(coxdata[,status_name]) =="double"){
     print("here")
     coxdata$record_status.ch1 <- coxdata[,status_name]
   }else{
     for (i in 1:length(rownames(coxdata))) {
-      coxdata$record_status.ch1[i] <- recordstatus(coxdata[,status_name][i]) 
+      coxdata$record_status.ch1[i] <- recordstatus(coxdata[,status_name][i])
     }
   }
   print("123")
-  
-  
-  
-  
+
+
+
+
   # numberic type conversion
   surv_time_name <- names(coxdata)[grep("time",names(coxdata))][1]
   coxdata[,surv_time_name] <- as.numeric(coxdata[,surv_time_name])
-  
-  
-  
+
+
+
   #2, test each gene independently via Cox regression
   f <- paste('Surv(',surv_time_name,',','record_status.ch1',')' ,'~ [*]')
   print(f)
-  
+
   fname <- paste(gseid,surv_time_name,sep = " ",collapse = "")
   fcheckname <- paste("./",fname,sep="",collapse = "")
   if(!file.exists(fcheckname)){
@@ -68,13 +82,13 @@ getCoxEfficient <- function(gseid){
       cores = 2,
       nestedParallel = FALSE,
       conflevel = 95)
-    
+
     write.csv(res,fname)
   }else{
     print("456")
     res <- read.csv(fname,header = T,stringsAsFactors = F)
   }
-  
+
   r<- list()
   r[[1]]<-res
   r[[2]]<-coxdata
@@ -93,10 +107,10 @@ getSurvData <- function(coxdata,res,sig_set,type = "survival"){
     sig2$logrank[i]<- res[b_i,]$LogRank
     sig2$pvalue[i]<- res[b_i,]$P
     sig2$hr[i]<- res[b_i,]$HR
-    
+
   }
   sig3 <- data.frame(geneid = unique(sig2$geneid),stringsAsFactors = F)
-  
+
   for (i in seq_along(sig3$geneid)) {
     cur <- sig2[which(sig2$geneid == sig3$geneid[i]),]
     sig3$sig[i] <- cur$sig[1]
@@ -106,23 +120,23 @@ getSurvData <- function(coxdata,res,sig_set,type = "survival"){
     sig3$hr[i]<- max(cur$hr)
   }
   sig2 <- sig3
-  
-  
-  
+
+
+
   #prepare survial data
   col_sel <- grep("ch1",colnames(coxdata))
-  
+
   sur_data2 <- coxdata[,col_sel]
   for (i in 1:length(sig2$sig)) {
     sig_index <- grep(sig2$sig[i],names(coxdata))
     probe_name <- as.character(sig2$geneid[i])
     sur_data2[,probe_name] <- coxdata[,sig_index]
-    
+
   }
-  
-  
-  
-  
+
+
+
+
   #calculate risk score
   sur_data3 <- sur_data2
   rs_sum <- numeric(length = length(rownames(sur_data3)))
@@ -130,15 +144,15 @@ getSurvData <- function(coxdata,res,sig_set,type = "survival"){
     cur_gene <- sur_data3[,sig2$geneid[i]]
     rs_sum <- rs_sum +  cur_gene*sig2$coeff[i]
   }
-  
+
   for (i in 1: length(rownames(sur_data3))) {
     risk_score <- rs_sum[i]
     sur_data3$rs.ch1[i] <- risk_score
   }
-  
-  
+
+
   highExpr <- median(sur_data3$rs)
-  
+
   for (i in 1: length(rownames(sur_data3))) {
     sur_data3$risk_status.ch1[i] <- ifelse(sur_data3$rs[i] >= highExpr, 'High','low')
   }
